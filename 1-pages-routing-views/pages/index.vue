ecff55d5b6c5f55e99a04ac668b6ffd51ce838f0
Pierre <pierre@Pierres-MBP.lan>
2019-04-04 19:02:17 +0200

    WIP: Pages, routing and views

---
+++ 1-pages-routing-views/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <logo></logo>
+    <br><br>
+    <h1>Test</h1>
+    <!-- <a href="/users">Users</a> -->
+    <!-- nuxt-link prevents reloading the entire page to act like a SPA -->
+    <!-- <router-link></router-link> VueJS version -->
+    <nuxt-link to="/users">Users</nuxt-link>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 1-pages-routing-views/pages/users/_id/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Page _id - Id is {{ $route.params.id }}</h1>
+</template>
