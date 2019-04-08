8915378b042d27a2f0fd6bcfa4d4daca5b913386
Pierre <pierre@Pierres-MacBook-Pro.local>
2019-04-08 19:03:09 +0200

    Communication with Back-End

---
+++ 4-config-plugins-modules/components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+    v-for="(post, index) in posts"
+    :key="index"
+    :id="post.id"
+    :is-admin="isAdmin"
+    :title="post.title"
+    :previewText="post.previewText"
+    :thumbnail="post.thumbnail" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview.vue';
+
+export default {
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    },
+    posts: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
